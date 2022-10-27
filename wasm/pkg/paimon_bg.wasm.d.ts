afc300603ab8fe6ba1568e060f9c0ff77e8b4e30
Haz Æ 41 <4405263+hazae41@users.noreply.github.com>
2022-10-27 10:32:24 +0200

    fix gitignore

---
+++ wasm/pkg/paimon_bg.wasm.d.ts
@@ -0,0 +1,24 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export function __wbg_paddingscheme_free(a: number): void;
+export function paddingscheme_new_pkcs1v15_sign_raw(): number;
+export function paddingscheme_new_pkcs1v15_sign_digest(a: number, b: number, c: number): number;
+export function __wbg_rsaprivatekey_free(a: number): void;
+export function rsaprivatekey_new(a: number, b: number): void;
+export function rsaprivatekey_from_pkcs1_der(a: number, b: number, c: number): void;
+export function rsaprivatekey_from_pkcs8_der(a: number, b: number, c: number): void;
+export function rsaprivatekey_to_pkcs1_der(a: number, b: number): void;
+export function rsaprivatekey_to_pkcs8_der(a: number, b: number): void;
+export function rsaprivatekey_to_public_key(a: number): number;
+export function rsaprivatekey_sign(a: number, b: number, c: number, d: number, e: number): void;
+export function __wbg_rsapublickey_free(a: number): void;
+export function rsapublickey_from_pkcs1_der(a: number, b: number, c: number): void;
+export function rsapublickey_from_public_key_der(a: number, b: number, c: number): void;
+export function rsapublickey_to_pkcs1_der(a: number, b: number): void;
+export function rsapublickey_to_public_key_der(a: number, b: number): void;
+export function rsapublickey_verify(a: number, b: number, c: number, d: number, e: number, f: number): number;
+export function __wbindgen_malloc(a: number): number;
+export function __wbindgen_add_to_stack_pointer(a: number): number;
+export function __wbindgen_free(a: number, b: number): void;
+export function __wbindgen_exn_store(a: number): void;
